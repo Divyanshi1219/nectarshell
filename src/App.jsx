96d98598258674214a610c05e44ea47b2ab844fa
Karan <karanmandal9654@gmail.com>
2026-09-23 18:02:55 +0530

    Add ScrollToTop and hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -27,6 +27,7 @@ import CashFlowManagement from "./pages/CashFlowManagement";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
 
       {/* COMMON HEADER */}
       <Header />
